27a1e7e765771f8c300464c290035d3596bdab7a
Marco Aurelio Pavini Mondini <marco.mondini@venturus.org.br>
2022-11-17 21:48:09 -0300

    added safe arguments gradle plugin

--- andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ andfun-kotlin-android-trivia-starter-code/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -39,6 +40,13 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener {
             it.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        //Receives the arguments from the bundle which has come from GameFragment
+        //this actions are made by SafeArguments gradle plugin
+        var args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, " +
+                "NumQuestions: ${args.numQuestions}",
+                Toast.LENGTH_LONG).show()
 
         return binding.root
     }
